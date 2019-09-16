c25ae7ce6e513d6a1d1bee886fd3ee85443d1f7a
lwrabbit <zhiming.smk@gmail.com>
2019-09-16 11:12:15 +0800

    fix pkg

--- greet.go
+++ greet.go
@@ -1,4 +1,4 @@
-package main
+package greet
 
 import (
 	"fmt"
@@ -8,8 +8,4 @@ import (
 // Hi returns a friendly greeting
 func Hi(name string) string {
 	return fmt.Sprintf("%s, %s", greetings.HI, name)
-}
-
-func main() {
-	fmt.Println(Hi("lwrabbit"))
 }
