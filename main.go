95c9c21dd11ef2cf3b0a0d64258e36e78d78433d
lwrabbit <zhiming.smk@gmail.com>
2019-09-16 10:57:03 +0800

    uodate to v2

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	 "github.com/lwrabbit/greetings"
+	 "github.com/lwrabbit/greetings/v2"
 )
 
 // Hi returns a friendly greeting
@@ -10,6 +10,12 @@ func Hi(name string) string {
 	return fmt.Sprintf("%s, %s", greetings.HI, name)
 }
 
+// Hello returns a friendly greeting
+func Hello(name string) string {
+	return fmt.Sprintf("%s, %s", greetings.HELLO, name)
+}
+
 func main() {
 	fmt.Println(Hi("lwrabbit"))
+	fmt.Println(Hello("lwrabbit"))
 }
